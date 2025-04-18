d8ef53ac987f84358ab3d4b2701c9e79431f7c64
LuisMoraEL <luismora045@outlook.com>
2025-04-18 14:29:03 -0600

    Primeros ejercicios de DLL

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,14 @@ public class Main {
     public static void main(String[] args) {
         DoubleLinkedList myDll = new DoubleLinkedList(8);
         myDll.append(76);
-        myDll.prepend( 17);
+        myDll.prepend(17);
+        myDll.append(78);
+        myDll.append(43);
+        myDll.insert(2, -45);
+        System.out.println("DLL after insert in 2");
+        myDll.printAll();
+        System.out.println("Removing node [1]: " + myDll.remove(1).value);
+        System.out.println("DLL after remove!");
         myDll.printAll();
     }
 }
--- src/datastructures/doublelinkedlist/DoubleLinkedList.java
+++ src/datastructures/doublelinkedlist/DoubleLinkedList.java
@@ -16,6 +16,93 @@ public class DoubleLinkedList {
         ++length;
     }
 
+    public void swapFirstLast() {
+        if (length < 2) return;
+        int temp = head.value;
+        head.value = tail.value;
+        tail.value = temp;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index > length - 1) return null;
+        if (index == 0) {
+            return removeFirst();
+        } else if (index == length - 1) {
+            return removeLast();
+        }
+        Node before = get(index - 1);
+        Node after = before.next.next;
+        Node current = before.next;
+        before.next = after;
+        after.prev = before;
+        current.prev = null;
+        current.next = null;
+        --length;
+        return current;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) {
+            return false;
+        } else if (index == 0){
+            prepend(value);
+            return true;
+        } else if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node prev = get(index - 1);
+        Node after = prev.next;
+        prev.next = newNode; //
+        newNode.prev = prev; //
+        after.prev = newNode; //
+        newNode.next = after; //
+        ++length;
+        return true;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp == null)
+            return false;
+        temp.value = value;
+        return true;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index > length - 1) return null;
+        Node temp = null;
+        if (index < length/2) {
+            temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        if (length == 1) {
+            head = null;
+            tail = null;
+            length = 0;
+            return temp;
+        }
+        head = head.next;
+        head.prev = null;
+        temp.next = null;
+        --length;
+        return temp;
+    }
+
     public void prepend(int value) {
         Node newNode = new Node(value);
         if (length == 0) {
@@ -102,6 +189,7 @@ public class DoubleLinkedList {
     }
 
     public void printAll() {
+        System.out.println("\tPrintAll!");
         System.out.println("head = " + head.value);
         System.out.println("tail = " + tail.value);
         System.out.println("length = " + length);
